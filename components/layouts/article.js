80e7e63c4d2febbe08d0c3734216e9806e66ca81
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-03-11 21:49:55 +0100

    holis

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Mimika Ackerman`
   return (
     <motion.article
       initial="hidden"
